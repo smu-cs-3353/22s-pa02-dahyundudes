869dd9a990fbf44ed23259916d3e0954e4d008fa
Cry1is <zachary.suzuki@gmail.com>
2022-03-02 11:21:18 -0600

    TODOs written

--- src/TestSort.h
+++ src/TestSort.h
@@ -10,11 +10,66 @@
 template <class T>
 class TestSort : public Sort<T> {
 public:
+    /**
+     * Default Constructor
+     */
     TestSort();
+    /**
+     * Copy Constructor
+     */
+    TestSort(const Sort<T>&);
+    /**
+     * Overloaded Constructor
+     * @param T* array of templated elements
+     */
     TestSort(T*);
+    /**
+     * Destructor
+     */
+    ~TestSort();
 
+    /**
+     * Sorts the data array using MiracleSort and returns the result
+     * @return T* array of templated elements
+     */
     T* sort();
 };
 
+template <class T>
+TestSort<T>::TestSort() {
+    this->data = nullptr;
+}
+
+template <class T>
+TestSort<T>::TestSort(const Sort<T>& other) {
+    if (sizeof(other.data) > 0) {
+        this->data = new T[sizeof(other.data)];
+        for (int i = 0; i < sizeof(other.data); i++)
+            this->data[i] = other.data[i];
+    }
+}
+
+template <class T>
+TestSort<T>::TestSort(T* other) {
+    if (sizeof(other > 0)) {
+        this->data = new T[sizeof(other)];
+        for (int i = 0; i < sizeof(other); i++)
+            this->data[i] = other[i];
+    }
+}
+
+template <class T>
+TestSort<T>::~TestSort() {
+    if (this->data != nullptr)
+        delete[] this->data;
+}
+
+template <class T>
+T* TestSort<T>::sort() {
+    TestSort<T> temp(*this);
+    //TODO: randomly shuffle the array in temp
+    //TODO: check if the array is sorted
+    //TODO: otherwise repeat
+}
 
 #endif //INC_22S_PA02_TESTSORT_H
