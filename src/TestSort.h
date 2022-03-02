bc95c0037462758dc4ea03e512b69e4bd8b59880
Cry1is <zachary.suzuki@gmail.com>
2022-03-02 11:25:18 -0600

    Implemented MiracleSort

--- src/TestSort.h
+++ src/TestSort.h
@@ -67,9 +67,16 @@ TestSort<T>::~TestSort() {
 template <class T>
 T* TestSort<T>::sort() {
     TestSort<T> temp(*this);
-    //TODO: randomly shuffle the array in temp
-    //TODO: check if the array is sorted
-    //TODO: otherwise repeat
+    bool sorted = false;
+    while (!sorted) {
+        sorted = true;
+        for (int i = 1; i < sizeof(temp.data); i++)
+            if (temp.data[i] < temp.data[i - 1]) {
+                sorted = false;
+                break;
+            }
+    }
+    return true;
 }
 
 #endif //INC_22S_PA02_TESTSORT_H
